b730708b5aa154451f7d65c6afcd24d80cebb9f7
Boris <bgrebennikovv@gmail.com>
2025-02-13 22:54:17 +0400

    method for finding free slots in basket

--- src/main/java/org/skypro/skyshop/basket/ProductBasket.java
+++ src/main/java/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,6 +1,10 @@
 package org.skypro.skyshop.basket;
 
+import org.skypro.skyshop.exceptions.BasketFullException;
 import org.skypro.skyshop.product.Product;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ProductBasket {
 
@@ -11,5 +15,13 @@ public class ProductBasket {
         if (capacity == null) capacity = 5;
         productsStore = new Product[capacity];
     }
+
+    private Integer findFreeBasketSlot() {
+        for (int i = 0; i < productsStore.length; i++) {
+            if (productsStore[i] != null) return i;
+        }
+        return null;
+    }
+
 
 }
