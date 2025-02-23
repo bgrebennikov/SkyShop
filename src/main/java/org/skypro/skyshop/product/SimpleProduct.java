3a7792bd2c81f26f967bfdc4470d90b726ce1c7b
Boris <bgrebennikovv@gmail.com>
2025-02-24 03:46:30 +0400

    add toStr, equals, and hashcode

--- src/main/java/org/skypro/skyshop/product/SimpleProduct.java
+++ src/main/java/org/skypro/skyshop/product/SimpleProduct.java
@@ -1,7 +1,5 @@
 package org.skypro.skyshop.product;
 
-import java.util.Objects;
-
 public class SimpleProduct extends Product {
 
     private final String title;
@@ -26,27 +24,6 @@ public class SimpleProduct extends Product {
     @Override
     public String getTitle() {
         return title;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "title='" + title + '\'' +
-                ", price=" + price +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SimpleProduct product = (SimpleProduct) o;
-        return Objects.equals(title, product.title) && Objects.equals(price, product.price);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, price);
     }
 
 }
